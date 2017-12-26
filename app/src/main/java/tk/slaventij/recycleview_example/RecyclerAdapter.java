6731f2e2ce5822722a695c4d4baffc7e944f654e
Slaventij Bilokur <svsq@outlook.com>
2017-12-26 17:27:02 +0200

    Working on Adapter and layout

--- app/src/main/java/tk/slaventij/recycleview_example/RecyclerAdapter.java
+++ app/src/main/java/tk/slaventij/recycleview_example/RecyclerAdapter.java
@@ -1,6 +1,8 @@
 package tk.slaventij.recycleview_example;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -10,19 +12,22 @@ class RecyclerAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
 
     private ArrayList<ModelItem> items = new ArrayList<>();
 
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new RecyclerViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.bind(items.get(position));
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public void addAll(List<ModelItem> fakeItems) {
